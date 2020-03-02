f09b837a082e419a0f256930da792c94ee0c2223
saad-allah <saad.allah.sae@gmail.com>
2020-03-02 16:55:50 +0100

    new site

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,19 @@
 import React from "react"
-
-import Layout from "../components/layout"
+import { Link } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import SEO from "../components/seo"
-
+import "../style/layout.css";
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <div className="_404" >
+    <SEO title="404 Not found" />
+    <h2>404</h2>
+    <h1>Nous ne trouvons pas ce que vous cherchez au juste</h1>
+    <p>N’ayez pas peur, vous pouvez revenir à la page d’accueil ou faire une nouvelle recherche.</p>
+    <Link cover direction="up" duration={1.5} bg="#c0181f"  to='/' className="nav-link">
+    Revenir à l’accueil
+            </Link>
+
+  </div>
 )
 
 export default NotFoundPage
